57b82108c2b4f55ca74b28c19f320a7023120f13
saber09130605 <1031703364@qq.com>
2024-07-10 10:26:07 +0800

    忽略dist文件夹

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response } from "./types/common";
-export * from "./types/common";
-export * from "./utils/utils";
-export declare const bg2CopyToClipboard: (text: string) => Response<void>;
--- dist/utils/utils.d.ts
+++
@@ -1,2 +0,0 @@
-import { Code, Response } from '../types/common';
-export declare function buildResponse<T>(code: Code, data?: T): Response<T>;
